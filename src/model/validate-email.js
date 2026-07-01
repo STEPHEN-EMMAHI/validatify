7de55621441362280e8407ef67682fc0522f0ae6
stephen emmahi nartey <emmahiofficial@gmail.com>
2026-07-01 11:12:21 -0400

    add email functionality

---
+++ src/model/validate-email.js
@@ -0,0 +1,13 @@
+export function validateEmail() {
+  const email = document.getElementById("email");
+  const inputChar = email.value;
+  const emailRegex = /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@gmail.com$/.test(inputChar);
+
+  if (emailRegex) {
+    email.classList.remove('border-red-500');
+    email.classList.add('border-green-500');
+  }else {
+    email.classList.remove('border-green-500');
+    email.classList.add('border-red-500');
+  }
+}
