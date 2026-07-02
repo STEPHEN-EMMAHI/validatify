d40065ee879a26591bc04423a8e0abdc61e476b1
stephen emmahi nartey <emmahiofficial@gmail.com>
2026-07-02 10:22:09 -0400

    add password structure and rules

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,4 +1,13 @@
 import { validateEmail } from "../model/validate-email.js";
+import {showPasswordRules, showOrHidePassword, clearPasswordRules} from "../model/validate-password.js";
 
 const email = document.getElementById("email");
 email.addEventListener("input", validateEmail);
+
+email.addEventListener("focus", clearPasswordRules );
+
+const password = document.getElementById("password");
+password.addEventListener("focus", showPasswordRules);
+
+const showHidePassword = document.getElementById("showHidePassword");
+showHidePassword.addEventListener("click", showOrHidePassword);
