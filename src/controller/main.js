e551a5a65feeeb4177867db8f0306a9645ec6c21
stephen emmahi nartey <emmahiofficial@gmail.com>
2026-07-03 12:24:47 -0400

    set password validation

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,5 +1,6 @@
 import { validateEmail } from "../model/validate-email.js";
-import {showPasswordRules, showOrHidePassword, clearPasswordRules} from "../model/validate-password.js";
+import {showPasswordRules, showOrHidePassword, clearPasswordRules, 
+    validatePasswordRules} from "../model/validate-password.js";
 
 const email = document.getElementById("email");
 email.addEventListener("input", validateEmail);
@@ -9,5 +10,7 @@ email.addEventListener("focus", clearPasswordRules );
 const password = document.getElementById("password");
 password.addEventListener("focus", showPasswordRules);
 
-const showHidePassword = document.getElementById("showHidePassword");
+const showHidePassword = document.getElementById("showHidePasswordBtn");
 showHidePassword.addEventListener("click", showOrHidePassword);
+
+password.addEventListener("input", validatePasswordRules);
--- src/model/validate-password.js
+++ src/model/validate-password.js
@@ -1,3 +1,4 @@
+/* Show Password Rules Function */
 export function showPasswordRules() {
     const passwordRules = document.getElementById("password-rules");
 
@@ -6,20 +7,66 @@ export function showPasswordRules() {
     }
 }
 
+/* Show or Hide Password Function */
 export function showOrHidePassword() {
     const password = document.getElementById("password");
+    const showOrHideBtn = document.getElementById("showHidePasswordBtn");
     
     if (password.type === "password") {
         password.type = "text";
-    }else if (password.type === "text") {
+        showOrHideBtn.textContent = "hide";
+    } else if (password.type === "text") {
         password.type = "password";
+        showOrHideBtn.textContent = "show";
     }
 }
 
+/* Clear Password Rules Function */
 export function clearPasswordRules() {
   const passwordRules = document.getElementById("password-rules");
   
   if (!passwordRules.classList.contains("hidden")) {
     passwordRules.classList.add("hidden");
   }
+}
+
+/* Validate Password Rules Function */
+export function validatePasswordRules() {
+    const password = document.getElementById("password");
+    const charactersLong = document.getElementById("char-long");
+    const upperCase = document.getElementById("upper");
+    const lowerCase = document.getElementById("lower");
+
+    // validate password length
+    if (password.value.length >= 8 && password.value.length <= 20) {
+        charactersLong.classList.add("hidden");
+        password.classList.remove("border-red-500");
+        password.classList.add("border-green-500");
+    } else {
+        charactersLong.classList.remove("hidden");
+        password.classList.remove("border-green-500");
+        password.classList.add("border-red-500");
+    }
+
+    if (password.value.length >= 1 && password.value.length < 8) {
+        charactersLong.textContent = "At least 8 characters long";
+    } else if (password.value.length > 20) {
+        charactersLong.textContent = "At most 20 characters long";
+    } else if (password.value.length === 0) {
+        charactersLong.textContent = "From 8 to 20 characters long";
+    }
+
+    // validate uppercase letter
+    if (/[A-Z]+/.test(password.value)) {
+        upperCase.classList.add("hidden");
+    }else {
+        upperCase.classList.remove("hidden");
+    }
+
+    // validate lowercase letter
+    if (/[a-z]+/.test(password.value)) {
+        lowerCase.classList.add("hidden");
+    }else {
+        lowerCase.classList.remove("hidden");
+    }
 }
