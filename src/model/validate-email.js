c6bc87fbad0a78b7c9cfd21c1a118bb1bce9e243
stephen emmahi nartey <emmahiofficial@gmail.com>
2026-07-04 20:26:44 -0400

    add submit button functionality, fix same email and password issues

--- src/model/validate-email.js
+++ src/model/validate-email.js
@@ -1,7 +1,7 @@
+/* Validate Email Function */
 export function validateEmail() {
   const email = document.getElementById("email");
-  const inputChar = email.value;
-  const emailRegex = /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@gmail.com$/.test(inputChar);
+  const emailRegex = /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@gmail.com$/.test(email.value);
 
   if (emailRegex) {
     email.classList.remove("border-red-500");
@@ -10,4 +10,23 @@ export function validateEmail() {
     email.classList.remove("border-green-500");
     email.classList.add("border-red-500");
   }
+
+  // check if password is the same as email
+  const checkPasswordAndEmail = document.getElementById("samePasswordAndEmail");
+  const password = document.getElementById("password");
+
+  if (password.value === email.value) {
+        checkPasswordAndEmail.classList.remove("hidden");
+        password.classList.remove("border-green-500");
+        password.classList.add("border-red-500");
+    } else {
+        checkPasswordAndEmail.classList.add("hidden");
+        password.classList.remove("border-red-500");
+
+        if (password.value !== "") {
+          password.classList.add("border-green-500");
+        } else {
+          password.classList.remove("border-red-500");
+        }
+    } 
 }
